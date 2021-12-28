bffffc36a2a829cd12ac04c7bfd922885a2053ec
Christian Beier <info@christianbeier.net>
2021-12-28 20:27:24 +0100

    Initial commit, assert fails

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use casbin::{CoreApi, DefaultModel, Enforcer, RbacApi};
+use sqlx_adapter::SqlxAdapter;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    let model = DefaultModel::from_file("casbin_model.conf").await?;
+    let adapter = SqlxAdapter::new(std::env::var("DATABASE_URL")?, 10).await?;
+    let mut enforcer = Enforcer::new(model, adapter).await?;
+
+    enforcer
+        .add_role_for_user("kurt", "data2_admin", None)
+        .await
+        .unwrap_or_default();
+
+    enforcer.delete_user("kurt").await?;
+    assert_eq!(
+        vec![String::new(); 0],
+        enforcer.get_roles_for_user("kurt", None)
+    );
+
+    Ok(())
+}
